1c6a35d813ad427db3238a1af0fc746a9c144521
David McGillicuddy <contact@djmcgill.co.uk>
2023-12-11 18:02:17 +0000

    o(n) d11 pass

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -1,57 +1,62 @@
-use fxhash::FxHashSet as HashSet;
-use std::cmp::{max, min};
+use std::fs::File;
+use std::time::Instant;
 
 // const INPUT: &[u8] = include_bytes!("../test.txt");
 const INPUT: &[u8] = include_bytes!("../real.txt");
 
+// thanks to manhatten distances, the x_dists and y_dists can be calculated independently
 fn main() {
-    let width = INPUT.splitn(2, |&c| c == b'\n').next().unwrap().len();
-    let mut empty_ys: HashSet<usize> = HashSet::default();
-    let mut empty_xs: HashSet<usize> = (0..width).collect();
-    let mut nodes = HashSet::default();
+    let start = Instant::now();
 
-    for (y, line) in INPUT.chunks_exact(width + 1).enumerate() {
-        let mut empty_y = true;
-        for (x, c) in line.into_iter().enumerate() {
-            if *c == b'#' {
-                empty_xs.remove(&x);
-                empty_y = false;
-                nodes.insert((x, y));
-            }
-        }
-        if empty_y {
-            empty_ys.insert(y);
-        }
-    }
+    let width = INPUT.splitn(2, |&c| c == b'\n').next().unwrap().len() as isize;
+    let height = INPUT.len() as isize / (width + 1);
 
-    // try shortcut: find the shortest edge in the graph, remove nodes, repeat
-    // this works by triangle inequality, probably
+    let ys_iter = (0..height).map(|y| (y, (0..width).map(move |x| y * (width + 1) + x)));
+    let xs_iter = (0..width).map(|x| (x, (0..height).map(move |y| y * (width + 1) + x)));
 
-    let mut edges = vec![];
-    for node_start in nodes.iter() {
-        for node_end in nodes.iter() {
-            if node_start < node_end {
-                let min_x = min(node_start.0, node_end.0);
-                let max_x = max(node_start.0, node_end.0);
-                let min_y = min(node_start.1, node_end.1);
-                let max_y = max(node_start.1, node_end.1);
-                let empty_x_count =
-                    empty_xs.iter().filter(|&&x| x > min_x && x < max_x).count() * (1000000 - 1);
-                let empty_y_count =
-                    empty_ys.iter().filter(|&&y| y > min_y && y < max_y).count() * (1000000 - 1);
-                let space_manhatten = max_x - min_x + empty_x_count + max_y - min_y + empty_y_count;
-                edges.push((space_manhatten, node_start, node_end));
-            }
-        }
-    }
-    edges.sort();
+    let p1_mult = 1;
+    let p1 = scan_dimension(ys_iter.clone(), p1_mult) + scan_dimension(xs_iter.clone(), p1_mult);
 
-    let mut chosen_edges: HashSet<((usize, usize), (usize, usize))> = HashSet::default();
-    let mut p1 = 0;
-    for (dist, start, end) in edges {
-        if chosen_edges.insert((*start, *end)) {
-            p1 += dist;
-        }
-    }
+    let p2_mult = 1000000 - 1;
+    let p2 = scan_dimension(ys_iter, p2_mult) + scan_dimension(xs_iter, p2_mult);
+
+    let end = Instant::now();
+
     println!("p1: {p1}");
+    println!("p2: {p2}");
+    println!("p1+p2: {:?}", end - start);
+}
+
+// okay let's say you have xs = [1,2,3,4]
+// then the xs dists are: 4-3 + 4-2 + 4-1 + 3-2 + 3-1 + 2-1
+// or: 4*(len-1) + 3*(len-2) + 2*(len-3) + 1*(len-4)
+//   - 4*(len-4) - 3*(len-3) + 2*(len-2) - 1(*len-1)
+//   = 4*(len-1) + 4*(4-len) + ...
+//   = 4*(len -1 + 4 - len) + ...
+//   = 4*3 + ...
+fn scan_dimension<Min: Iterator<Item = isize>, Maj: Iterator<Item = (isize, Min)>>(
+    iter: Maj,
+    mult: isize,
+) -> isize {
+    let mut ys = vec![];
+    let mut empties = 0isize;
+
+    // hmm how can I make this a list comprehension
+    for (maj, x_iter) in iter {
+        let maj_count = x_iter.filter(|&ix| INPUT[ix as usize] == b'#').count();
+        if maj_count == 0 {
+            empties += 1;
+        } else {
+            for _ in 0..maj_count {
+                ys.push(maj + empties * mult);
+            }
+        }
+    }
+
+    let len = ys.len() as isize;
+    ys.iter()
+        .enumerate()
+        .rev()
+        .map(|(n, y)| *y * ((2 * n as isize + 1) - len))
+        .sum()
 }
